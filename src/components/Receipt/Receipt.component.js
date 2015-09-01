2c258d8857630dc1407764cc54033ac8fd2faf17
Rikke Lose <rikkelose@dhcp106.dbc.dk>
2015-09-01 11:16:29 +0200

    aading link back to work

--- src/components/Receipt/Receipt.component.js
+++ src/components/Receipt/Receipt.component.js
@@ -35,18 +35,23 @@ const Receipt = React.createClass({
     const title = this.props.receipt.title;
     const creator = this.props.receipt.creator;
     const type = this.props.receipt.type;
+    const ids = this.props.receipt.ids.split(',');
 
     let orderInfo = title + ' (' + type + ')';
 
     if (creator !== '') {
       orderInfo = creator + ': ' + title + ' (' + type + ')';
     }
+
+    const backLink = 'work?id=' + ids[0];
+
     if (store[this.props.receipt.ids.toString()] === true) {
       this.setState({
         orderPlaced: true,
         headline: 'Vi har modtaget din bestilling på:',
         orderInfo: orderInfo,
-        libraryInfo: 'Du vil få besked fra dit bibliotek (' + this.props.receipt.pickupAgency + '), når materialet er klar til afhentning.'
+        libraryInfo: 'Du vil få besked fra dit bibliotek (' + this.props.receipt.pickupAgency + '), når materialet er klar til afhentning.',
+        backLink: <a className="back-button button" href={backLink}>Afslut</a>
       });
     }
     else {
@@ -54,19 +59,20 @@ const Receipt = React.createClass({
         orderPlaced: false,
         headline: 'Bestilling af følgende materiale mislykkedes:',
         orderInfo: orderInfo,
-        libraryInfo: ''
+        libraryInfo: '',
+        backLink: <a className="back-button button" href={backLink}>Gå tilbage</a>
       });
     }
   },
 
   render() {
-
     return (<div className='receipt--container'>
         <div className='receipt small-12 medium-6 large-4'>
           <div className="receipt--info">
             <div className="receipt--headline">{this.state.headline}</div>
             <div className="receipt--bibliographic">{this.state.orderInfo}</div>
             <div className="receipt--library">{this.state.libraryInfo}</div>
+            <div className="receipt--back-link">{this.state.backLink}</div>
           </div>
         </div>
       </div>
