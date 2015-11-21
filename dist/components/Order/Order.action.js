449116ebf8d9d1ed30a82e1491cdf5179da53860
Jonas Peter Hyatt <jph@dbc.dk>
2015-11-21 09:50:33 +0100

    cleaning dist from master

--- dist/components/Order/Order.action.js
+++
@@ -1,7 +0,0 @@
-'use strict';Object.defineProperty(exports, '__esModule', { value: true });function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { 'default': obj };}var _reflux = require(
-
-'reflux');var _reflux2 = _interopRequireDefault(_reflux);
-
-var OrderActions = _reflux2['default'].createAction({ children: ['updated', 'failed'] });exports['default'] = 
-
-OrderActions;module.exports = exports['default'];
